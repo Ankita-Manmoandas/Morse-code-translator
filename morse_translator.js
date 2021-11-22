60ce4dcca16ac514ff2ca4ff4b44161926db5b1e
Ankita Manmoandas <ankitamanmoandas@Ankitas-MacBook-Pro.local>
2021-11-22 13:00:33 +0000

    added functions for letters a-j

--- morse_translator.js
+++ morse_translator.js
@@ -1,6 +1,30 @@
 export const input = (letter) => {
-  if (letter=="a") {
-    return ".-";
-  }
+   if (letter=="a") {
+    return (".-");
+  } else if (letter== "b") {
+    return ("-...");
+  } else if (letter== "c") {
+    return ("-.-."); 
+  } else if (letter== "d") {
+    return ("-.."); 
+  } else if (letter== "e") {
+    return ("."); 
+  } else if (letter== "f") {
+    return ("..-."); 
+  } else if (letter== "g") {
+    return ("--."); 
+  } else if (letter== "h") {
+    return ("...."); 
+  } else if (letter== "i") {
+      return ("..");
+  } else if (letter== "j") {
+    return (".---");
+   } else if (letter== "i") {
+      return ("..");
+
+
+
+
+
 
 }
