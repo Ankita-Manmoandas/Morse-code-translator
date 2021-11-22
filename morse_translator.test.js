8b8dfa7f88b6a6739ac1ee1be6192eb1fd4f65a6
Ankita Manmoandas <ankitamanmoandas@Ankitas-MacBook-Pro.local>
2021-11-22 14:22:53 +0000

    finished tests for alphabet

--- morse_translator.test.js
+++ morse_translator.test.js
@@ -9,22 +9,31 @@ const { input } = require("./morse_translator");
 
   //describe
   describe ("translate single letters to morse", ()=> {
-    it ("should translate a to .- ", ()=> {
+    it ("should translate a to .- ", () => {
       const checkResult= input ("a")
       expect (checkResult).toBe (".-");
 
 
     })
-    it ("should translate i to .. ", ()=> {
+    it ("should translate i to .. ", () => {
+      const checkResult = input ("i")
+      expect (checkResult).toBe ("..")
     
       
     })
-    it ("should translate c to -.-. ", ()=> {
+    it ("should translate c to -.-. ", () => {
+      const checkResult = input ("c") 
+      expect (checkResult).toBe ("-.-.")
     
       
     })
-    it ("should translate o to ---", ()=>{
+    it ("should translate o to ---", () => {
+      const checkResult = input ("o") 
+      expect (checkResult).toBe ("---")
+    
 
     })
 
   })
+
+ 
